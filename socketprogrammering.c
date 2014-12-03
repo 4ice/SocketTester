563092ca30cae7404ff1d56b9497d11030a9480b
David Andersson <da.ch.andersson@gmail.com>
2014-12-03 12:42:25 +0100

    update 2/12

--- socketprogrammering.c
+++ socketprogrammering.c
@@ -24,14 +24,25 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+#include <linux/if_link.h>
 
 void server();
 void client();
 void readInput();
-//char *splitInput[](char input[]);
 char **getCommand();
+void cmdClientHelp();
+void cmdServerHelp();
+char *getIp();
+void sendPacket(int protocol, char *host, int portno, int packetSize, long fileSize);
+void recieveMeessage(int protocol, int portno, int size);
 
 
+//for the use of bool
+typedef int bool;
+#define true 1;
+#define false 0;
 
 
 
@@ -43,7 +54,7 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-	if(argc < 3) {
+	if(argc < 2) {
 		fprintf(stderr, "ERROR: the amount of parameters must be atleast 2");
 		exit(1);
 	}
@@ -59,102 +70,163 @@ int main(int argc, char *argv[])
 
 void server(int argc, char *argv[])
 {
-	printf("this is a server!!\n");
+	printf("this is a server!!\nNow tell me what you want to do, if you need help, write \"--help\"\n");
+	char **commands;
+	commands = getCommand();
 
-	int sockfd, newsockfd, portno;
-	socklen_t clilen;
-	char buffer[256];
-	struct sockaddr_in serv_addr, cli_addr;
-	int n;
-	if (argc < 3) {
-		fprintf(stderr, "ERROR, no port provided\n");
-		exit(1);
+
+	int i = 0;
+	int nrOfCommands = 0;
+	bool quit = false;
+
+	while(!quit && commands[i] != NULL)
+	{
+		nrOfCommands++;
+		//help
+		if(strcmp(commands[i], "--help\n") == 0)
+		{
+			cmdServerHelp();		
+			i++;
+		}
+		//Listen
+		else if(strcmp(commands[i], "--listen") == 0)
+		{
+			i++;
+			if(commands[i+2] == NULL)
+			{
+				error("ERROR: to few parameters");
+				commands[i] = NULL;
+			}
+			else
+			{
+				//TCP
+				if(strcmp(commands[i], "t") == 0)
+				{
+					recieveMeessage(1, strtol(commands[i+1], NULL, 10), strtol(commands[i+2], NULL, 10));
+					i = i+2;
+				}
+				else if(strcmp(commands[i], "u") == 0)
+				{
+					recieveMeessage(0, strtol(commands[i+1], NULL, 10), strtol(commands[i+2], NULL, 10));
+					i = i+2;
+				}
+				else
+					error("ERROR: not a valid protocol");
+			}
+
+		}
+	 	else if(strcmp(commands[i], "--quit\n") == 0)
+	    	{
+		    	quit = true;
+		    	exit(0);
+	    	}
+	    	//unknown command
+	    	else
+	    	{
+		    	printf("Unknown command, please use '--help' for help\n");
+		    	commands[i] = NULL;
+	    	}
+
+
+	    
+	    	if(commands[i] == NULL && !quit)
+	    	{
+		    	//Free memory
+		    	/*int k = 0;
+		    	for(k; k < nrOfCommands; k++)
+		    	{
+			    	free(commands[k]);
+		    	}
+		    	free(commands);
+		    	*/
+		    	commands = getCommand();
+		    	i = 0;
+		    	nrOfCommands = 0;
+	    	}
 	}
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0)
-		error("ERROR opening socket");
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	portno = atoi(argv[2]);
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(portno);
-	if (bind(sockfd, (struct sockaddr *) &serv_addr,
-				sizeof(serv_addr)) < 0)
-		error("ERROR on binding");
-	listen(sockfd, 5);
-	clilen = sizeof(cli_addr);
-	newsockfd = accept(sockfd,
-			(struct sockaddr *) &cli_addr,
-			&clilen);
-	if (newsockfd < 0)
-		error("ERROR on accept");
-	bzero(buffer, 256);
-	n = read(newsockfd, buffer, 255);
-	if (n < 0)
-		error("ERROR reading from socket");
-	printf("Here is the message: %s\n", buffer);
-	n = write(newsockfd, "I got your message", 18);
-	if ( n < 0)
-		error("ERROR writing to socket");
-	close(newsockfd);
-	close(sockfd);
+
 }
 
 void client(int argc, char *argv[])
 {
-    char input[64];
-	printf("this is a client!!\n Now tell me what you want to do, if you need help, write \"help\"\n");
-    //fgets(input, sizeof(input), stdin);
-    char **commands;
+    	char input[64];
+    	printf("this is a client!!\nNow tell me what you want to do, if you need help, write \"--help\"\n");
+    	char **commands;
+    	commands = getCommand();
 
-    commands = getCommand();
 
-    int i = 0;
-    while(commands[i] != NULL)
-    {
-        printf("%s\n", commands[i]);
-        i++;
-    }
+    
 
-    printf(input);
+    	int i = 0;
+    	int nrOfCommands = 0;
+    	bool quit = false;
+    	while(!quit && commands[i] != NULL)
+    	{
+	    	nrOfCommands++;
+	    	//help
+	    	if(strcmp(commands[i], "--help\n") == 0)
+	    	{
+		    	cmdClientHelp();
+		    	i++;
+	    	}
+	    	//send
+	    	else if(strcmp(commands[i], "--send") == 0)
+	    	{
+		    	i++;		    
+		    	if(commands[i+4] == NULL)
+		    	{
+			    	error("ERROR: to few parameters");
+			    	commands[i] = NULL;
+		    	}
+		    	else
+		    	{
+			    	//TCP
+			    	if(strcmp(commands[i], "t") == 0)
+			    	{
+				    	sendPacket(1, commands[i+1], strtol(commands[i+2], NULL, 10), strtol(commands[i+3], NULL, 10), strtol(commands[i+4], NULL, 10));
+				    	i = i+5;
+			    	}
+			    	//UDP
+			    	else if(strcmp(commands[i], "u") == 0)
+			    	{
+				    	sendPacket(0, commands[i+1], strtol(commands[i+2], NULL, 10), strtol(commands[i+3], NULL, 10), strtol(commands[i+4], NULL, 10));
+				    	i = i+5;
+			    	}
+			    	else
+				    	error("ERROR: not a valid protocol");
+					    
+		    	}
+	    	}
+	    	//quit
+	    	else if(strcmp(commands[i], "--quit\n") == 0)
+	    	{
+		   	quit = true;
+		    	exit(0);
+	    	}
+	    	//unknown command
+	    	else
+	    	{
+		    	printf("Unknown command, please use '--help' for help\n");
+		    	commands[i] = NULL;
+	    	}
 
-	int sockfd, portno, n;
-	struct sockaddr_in serv_addr;
-	struct hostent *server;
 
-	char buffer[256];
-	if (argc < 4) {
-		fprintf(stderr, "some parameter is missing\n");
-		exit(0);
-	}
-	portno = atoi(argv[3]);
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0)
-		error("ERROR opening socket");
-	server = gethostbyname(argv[2]);
-	if (server == NULL) {
-		fprintf(stderr, "ERROR, no such host\n");
-		exit(0);
-	}
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	bcopy((char *)server->h_addr,
-	     (char *)&serv_addr.sin_addr.s_addr,
-	     server->h_length);
-	serv_addr.sin_port = htons(portno);
-	if (connect(sockfd,(struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-		error("ERROR connecting");
-	printf("Please enter the message: ");
-	bzero(buffer, 256);
-	fgets(buffer, 255, stdin);
-	n = write(sockfd, buffer, strlen(buffer));
-	if (n<0)
-		error("ERROR writing to socket");
-	bzero(buffer, 256);
-	if (n<0)
-		error("ERROR reading from socket");
-	printf("%s\n", buffer);
-	close(sockfd);
+	    
+	    	if(commands[i] == NULL && !quit)
+	    	{
+		    	//Free memory
+		    	/*int k = 0;
+		    	for(k; k < nrOfCommands; k++)
+		    	{
+			    	free(commands[k]);
+		    	}
+		    	free(commands);
+		    	*/
+		    	commands = getCommand();
+		    	i = 0;
+		    	nrOfCommands = 0;
+	    	}
+    	}   
 }
 
 char **getCommand()
@@ -177,4 +249,192 @@ char **getCommand()
 
     commandArray[command_num] = NULL;  /*very imp - adds null in the last position*/
     return commandArray;
+}
+
+
+
+void cmdClientHelp()
+{
+	printf("****\nHELP\n****\n");
+	printf("--help \t\t\t\t\tShows this help text.\n");
+
+	printf("--send [t/u} [hostIP] [port] [size] \tSends a packet to given host, port and packetsize\n");
+	printf("\tt \t\t\t\tTCP protocol\n");
+	printf("\tu \t\t\t\tUDP protocol\n\n");
+	printf("--quit \t\t\t\t\tclose the program\n");
+}
+
+void cmdServerHelp()
+{
+	printf("****\nHELP\n****\n");
+	printf("--help \t\t\t\t\tShows this help text.\n");
+
+	printf("--listen [t/u} [port] [size]\t\tListen for packet, given protocol and port\n");
+	printf("\tt \t\t\t\tTCP protocol\n");
+	printf("\tu \t\t\t\tUDP protocol\n\n");
+	printf("--quit \t\t\t\t\tclose the program\n");
+}
+
+char *getIp()
+{
+	char *ipAddress;
+
+	struct ifaddrs *addrs, *tmp;
+	getifaddrs(&addrs);
+	tmp = addrs;
+	while(tmp)
+	{
+		if (tmp->ifa_addr && tmp->ifa_addr->sa_family == AF_INET && strcmp(tmp->ifa_name, "eno1") == 0)
+		{
+			struct sockaddr_in *pAddr = (struct sockaddr_in *) tmp->ifa_addr;
+			ipAddress = inet_ntoa(pAddr->sin_addr);
+		}
+		tmp = tmp->ifa_next;
+	}
+
+	freeifaddrs(addrs);
+
+	return ipAddress;
+}
+//NOTE: TCP == 1, UDP == 0
+void sendPacket(int protocol, char *host, int portno, int packetSize, long fileSize)
+{
+    int sockfd, n;
+
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+
+
+
+    char buffer[packetSize];
+    memset(buffer, 'a', sizeof(buffer));
+    
+    //Open a socket, either UDP or TCP
+    if(protocol == 0)
+    {
+	    //UDP
+	    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    }
+    else
+    {
+	    //TCP
+	    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    }
+    //Check if the socket could be opened
+    if (sockfd < 0)
+    {
+	    error("ERROR opening socket");
+    }
+
+    //Get the server
+    server = gethostbyname(host);
+    if (server == NULL) {
+	    fprintf(stderr, "ERROR, no such host\n");
+	    exit(0);
+    }
+    //Set all values in serv_addr to zero
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    //Use IPv4 Internet protocols
+    serv_addr.sin_family = AF_INET;
+
+    //copy server->h_addr to serv_addr.sin_addr.s_addr
+    bcopy((char *)server->h_addr,
+		    (char *)&serv_addr.sin_addr.s_addr,
+       		    server->h_length);
+    //Add the port number
+    serv_addr.sin_port = htons(portno);
+    //Try to connect to the host
+    if (connect(sockfd,(struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+	    error("ERROR connecting");
+    while(fileSize > 0)
+    {
+	    n = write(sockfd, buffer, strlen(buffer));
+	    if(n < 0)
+		    error("ERROR writing to socket");
+	    fileSize = fileSize-packetSize;
+    }
+    n = write(sockfd, "b", 1);
+    if(n < 0)
+	    error("ERROR writing to socket");
+
+    //Close the connection
+    close(sockfd);
+}
+
+void recieveMeessage(int protocol, int portno, int size)
+{
+	time_t ticks;
+
+    	printf("-----Server information-----\n");
+
+	struct ifaddrs *addrs, *tmp;
+
+	getifaddrs(&addrs);
+	tmp = addrs;
+	while(tmp)
+	{
+		if(tmp->ifa_addr && tmp->ifa_addr->sa_family == AF_INET)
+		{
+			struct sockaddr_in *pAddr = (struct sockaddr_in *)tmp->ifa_addr;
+		        printf("%s:\t\t%s\n", tmp->ifa_name, inet_ntoa(pAddr->sin_addr));
+		}
+		tmp = tmp->ifa_next;
+	}
+	freeifaddrs(addrs);
+    	char *protocolName;
+    	if(protocol == 0)
+	    	protocolName = "UDP";
+    	else
+	    	protocolName = "TCP";
+
+
+    	printf("Protocol:\t%s\n", protocolName);
+	printf("Port:\t\t%i\n", portno);
+	printf("PacketLength\t%i\n", size);
+	printf("----------------------------\n");
+
+
+	int sockfd, newsockfd, clilen;
+        char buffer[size];
+        struct sockaddr_in serv_addr, cli_addr;
+        int n;
+
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0) 
+           error("ERROR opening socket");
+	
+	memset(&serv_addr, 0, sizeof(struct sockaddr_in));
+
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_addr.s_addr = INADDR_ANY;
+        serv_addr.sin_port = htons(portno);
+        
+	if (bind(sockfd, (struct sockaddr *) &serv_addr,
+                 sizeof(serv_addr)) < 0) 
+                 error("ERROR on binding");
+        
+	listen(sockfd,5);
+        
+	clilen = sizeof(cli_addr);
+	ticks = time(NULL);
+	bool keepListen = true;
+	while(keepListen)
+	{
+        	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+		
+		memset(buffer, 0, size);
+        	n = read(newsockfd,buffer,size-1);
+		if(strcmp(buffer, "b") == 0)
+		{
+			keepListen = false;
+		}
+		printf("%s\n\n", buffer);
+	}
+
+        printf("Here is the message: %s\n",buffer);
+
+	close(newsockfd);
+	close(sockfd);
 }
